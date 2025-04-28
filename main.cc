8461f4d0dcc3fb7e3303190143d32728a310bfa9
Chase Chemero <cchemero@dons.usfca.edu>
2025-04-27 19:21:18 -0700

    .

--- main.cc
+++ main.cc
@@ -1,49 +1,16 @@
-// ns-3 core modules
-#include "ns3/ipv4-header.h"
-#include "ns3/packet.h"
-#include "ns3/tcp-header.h"
-#include "ns3/udp-header.h"
 #include "ns3/core-module.h"
-
-// ns-3 core modules
-#include "ns3/ipv4-header.h"
-#include "ns3/packet.h"
-#include "ns3/tcp-header.h"
-#include "ns3/udp-header.h"
-#include "ns3/core-module.h"
-
-#include "main.h"
-
-#include "source-ip-address.h"
+// #include "test-destination-ip-address.cc" 
 
 using namespace ns3;
 
-// Standard C++ entry point for test programs
 int main(int argc, char *argv[])
 {
-    std::cout << "Running SourceIPAddress Test..." << std::endl;
+  CommandLine cmd;
+  cmd.Parse(argc, argv);
 
-    Ptr<Packet> packet = Create<Packet>();
+  std::cout << "Starting DiffServ main..." << std::endl;
 
-    Ipv4Header ipv4Header;
-    ipv4Header.SetSource(Ipv4Address("192.168.1.100"));
-    ipv4Header.SetDestination(Ipv4Address("192.168.1.200"));
-    ipv4Header.SetProtocol(6); // TCP
+//   TestDestinationIpAddressMatch();
 
-    packet->AddHeader(ipv4Header);
-
-    SourceIPAddress filter(Ipv4Address("192.168.1.100"));
-
-    bool matched = filter.Match(packet);
-
-    if (matched)
-    {
-        std::cout << "✅ Source IP matched!" << std::endl;
-    }
-    else
-    {
-        std::cout << "❌ Source IP did not match." << std::endl;
-    }
-
-    return 0;
+  return 0;
 }
