23ddc8476879ef865ab61de258256c453e592f35
Chase Chemero <cchemero@dons.usfca.edu>
2025-04-29 20:54:44 -0700

    Debug.

--- diff-serv.h
+++ diff-serv.h
@@ -1,82 +1,34 @@
 #ifndef DIFF_SERV_H
 #define DIFF_SERV_H
 
-#include "traffic-class.h"
 #include "ns3/queue.h"
-#include "ns3/packet.h"
+#include "traffic_class.h"
 
-namespace ns3 {
+using namespace ns3;
 
-/**
- * \ingroup diffserv
- * \brief DiffServ class to manage packets and queues.
- */
 class DiffServ : public Queue<Packet>
 {
+    public:
+        DiffServ();
 
-public:
-    DiffServ();
+        bool Enqueue(Ptr<Packet> p) override;
+        Ptr<Packet> Dequeue() override;
+        Ptr<Packet> Remove() override;
+        Ptr<const Packet> Peek() const override;
+        virtual Ptr<const Packet> Schedule() const = 0;
+        virtual uint32_t Classify(Ptr<Packet> p);
+        virtual void AddQueue(TrafficClass *q);
 
-       /**
-     * \brief Add a queue to the DiffServ class.
-     * \param queue The TrafficClass object to be added.
-     */
-    virtual void AddQueue(TrafficClass* queue);
+    protected:
+        std::vector<TrafficClass*> GetQueues() const;
 
-    /**
-     * \brief Enqueue a packet into the DiffServ queue.
-     */
-    bool Enqueue(Ptr<Packet> pkt) override;
+    private:
+        std::vector<TrafficClass*> q_class;
 
-    /** 
-     * \brief Dequeue a packet from the DiffServ queue.
-     * \returns The dequeued packet.
-     */
-    Ptr<Packet> Dequeue() override;
-
-    /** 
-     * \brief Remove a packet from the DiffServ queue.
-     * \returns The removed packet.
-     */
-    Ptr<Packet> Remove() override;
-
-    /**
-     * \brief Peek at the front packet in the DiffServ queue.
-     * \returns The front packet without removing it.
-     * \note This function does not modify the queue.
-     */
-    Ptr<const Packet> Peek() const override;
-
-    /**
-     * Primary Functions for DiffServ ( Schedule, Classify )
-     */
-
-    /**
-     * \brief Classify a packet based on its properties.
-     * \returns The classification result.
-     */
-    virtual uint32_t Classify(Ptr<Packet> pkt);
-
-    /**
-     * \brief Schedule the next packet to be sent.
-     * \returns The scheduled packet.
-     */
-    virtual Ptr<const Packet> Schedule() const = 0;
-
-
-protected:
-    std::vector<TrafficClass*> GetQueues() const;
-
-private:
-    std::vector<TrafficClass*> q_class;
-
-    Ptr<const Packet> ExecutePeek() const;
-    bool ExecuteEnqueue(Ptr<Packet> pkt);
-    Ptr<Packet> ExecuteDequeue();
-    Ptr<Packet> ExecuteRemove();
-    
+        bool DoEnqueue(Ptr<Packet> p);
+        Ptr<Packet> DoDequeue();
+        Ptr<Packet> DoRemove();
+        Ptr<const Packet> DoPeek() const;
 };
-
-} // namespace ns3
 
 #endif // DIFF_SERV_H
