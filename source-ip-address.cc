f28cbe8fbceb97cc122b33f6aca88bdd392b6f8c
Chase Chemero <cchemero@dons.usfca.edu>
2025-05-07 00:45:33 -0700

    Mini Cleanup Final - 2.5 - Port Not 3000

--- source-ip-address.cc
+++ source-ip-address.cc
@@ -1,44 +1,48 @@
 #include "ns3/ipv4-header.h"
 #include "source-ip-address.h"
 #include "ns3/ppp-header.h"
+#include "ns3/log.h"
+
+NS_LOG_COMPONENT_DEFINE("SourceIPAddress");  
 
 namespace ns3 {
+    /**
+     * \ingroup diffserv
+     * \brief Constructor for SourceIPAddress.
+     * \param sourceIp The source IP address to match.
+     */
+    SourceIPAddress::SourceIPAddress(Ipv4Address sourceIp): m_sourceIp(sourceIp) {}
 
-/**
- * \ingroup diffserv
- * \brief Constructor for SourceIPAddress.
- * \param sourceIp The source IP address to match.
- */
-SourceIPAddress::SourceIPAddress(Ipv4Address sourceIp)
-  : m_sourceIp(sourceIp) {}
-
-/**
- * \ingroup diffserv
- * \brief Match the packet against the source IP address.
- * \param pkt The packet to check.
- * \return true if the packet matches, false otherwise.
- */
-bool
-SourceIPAddress::Match(Ptr<Packet> pkt) const
-{
-    Ipv4Header ipv4Header;
-    Ptr<Packet> packetCopy = pkt->Copy();
-
-    PppHeader pppHeader;
-
-    if (!packetCopy->RemoveHeader(pppHeader))
+    /**
+     * \ingroup diffserv
+     * \brief Match the packet against the source IP address.
+     * \param pkt The packet to check.
+     * \return true if the packet matches, false otherwise.
+     */
+    bool
+    SourceIPAddress::Match(Ptr<Packet> pkt) const
     {
-        std::cout << "[PortMatch] Failed to remove PPP header" << std::endl;
-        return false;
+        // Make a copy of the packet to avoid modifying the original
+        Ptr<Packet> packetCopy = pkt->Copy();
+
+        // Use Headers to remove the PPP header and check the source IP
+        Ipv4Header ipv4Header;
+        PppHeader pppHeader;
+
+        if (!packetCopy->RemoveHeader(pppHeader))
+        {
+            NS_LOG_WARN("Packet does not contain PPP header");
+            return false;
+        }
+
+        // If we can't access the header, return false
+        if (!packetCopy->PeekHeader(ipv4Header))
+        {
+            NS_LOG_WARN("Packet does not contain IPv4 header");
+            return false;
+        }
+
+        // Check if the source IP address matches
+        return ipv4Header.GetSource() == m_sourceIp;
     }
-
-    // If we can't access the header, return false
-    if (!packetCopy->PeekHeader(ipv4Header))
-    {
-        return false;
-    }
-
-    return ipv4Header.GetSource() == m_sourceIp;
-}
-
 } // namespace ns3
--- source-ip-address.h
+++ source-ip-address.h
@@ -6,32 +6,30 @@
 #include "filter-element.h"
 
 namespace ns3 {
-
-/**
- * \ingroup diffserv
- * \brief Class to match source IP address.
- */
-class SourceIPAddress : public FilterElement
-{
-
-public:
     /**
-     * \brief Constructor.
-     * \param sourceIp The source IP address to match.
+     * \ingroup diffserv
+     * \brief Class to match source IP address.
      */
-    SourceIPAddress(Ipv4Address sourceIp);
+    class SourceIPAddress : public FilterElement
+    {
 
-    /**
-     * \brief Match the packet against the source IP address.
-     * \param pkt The packet to check.
-     * \return true if the packet matches, false otherwise.
-     */
-    bool Match(Ptr<Packet> pkt) const override;
+    public:
+        /**
+         * \brief Constructor.
+         * \param sourceIp The source IP address to match.
+         */
+        SourceIPAddress(Ipv4Address sourceIp);
 
-private:
-    Ipv4Address m_sourceIp;
-};
+        /**
+         * \brief Match the packet against the source IP address.
+         * \param pkt The packet to check.
+         * \return true if the packet matches, false otherwise.
+         */
+        bool Match(Ptr<Packet> pkt) const override;
 
+    private:
+        Ipv4Address m_sourceIp;
+    };
 } // namespace ns3
 
 #endif // SOURCE_IP_ADDRESS_H
