d58d22634de7ac73f653062d28c24c87f92efe8a
Chase Chemero <cchemero@dons.usfca.edu>
2025-05-07 22:03:16 -0700

    Fixing Traffic Class.

--- traffic-class.cc
+++ traffic-class.cc
@@ -3,226 +3,226 @@
 #include "filter.h"
 #include "traffic-class.h"
 
-using namespace ns3;
-
 NS_LOG_COMPONENT_DEFINE("TrafficClass");
 
-/**
- * \ingroup diffserv
- * \brief Constructor for TrafficClass.
- *
- * Initializes the traffic class with default values.
- */
-TrafficClass::TrafficClass() noexcept: m_packets(0), m_maxPackets(100), m_weight(0), m_priorityLevel(0), m_isDefault(false) {}
+using namespace ns3 {
+    /**
+     * \ingroup diffserv
+     * \brief Constructor for TrafficClass.
+     *
+     * Initializes the traffic class with default values.
+     */
+    TrafficClass::TrafficClass(): m_packets(0), m_maxPackets(100), m_weight(0), m_priorityLevel(0), m_isDefault(false) {}
 
-/**
- * \ingroup diffserv
- * \brief Getter for is default
- */
-bool TrafficClass::GetIsDefault() const
-{
-    NS_LOG_FUNCTION(this);
-    return m_isDefault;
-}
-
-/**
- * \ingroup diffserv
- * \brief Setter for is default
- */
-void TrafficClass::SetIsDefault(bool default_queue)
-{
-    NS_LOG_FUNCTION(this);
-    m_isDefault = default_queue;
-}
-
-/** 
- * \ingroup diffserv
- * \brief Enqueues a packet into the traffic class.
- */
-bool TrafficClass::Enqueue(Ptr<Packet> pkt)
-{
-    // Add the packet to the queue if it is not full
-    if (m_packets < m_maxPackets)
+    /**
+     * \ingroup diffserv
+     * \brief Getter for is default
+     */
+    bool TrafficClass::GetIsDefault() const
     {
-        m_queue.push(pkt);
-        m_packets++;
-
-        return true;
+        NS_LOG_FUNCTION(this);
+        return m_isDefault;
     }
 
-    NS_LOG_WARN("Queue is full. Packet not enqueued.");
-    return false;
-}
-
-/**
- * \ingroup diffserv
- * \brief Removes a packet from the traffic class.
- * \details Pops the next packet but does *not* fire the "served" trace logging.
- * From my research this is the main difference between Dequeue() and Remove().
- */
-Ptr<Packet> TrafficClass::Remove()
-{
-    // Return null pointer on empty queue
-    if (m_queue.empty())
+    /**
+     * \ingroup diffserv
+     * \brief Setter for is default
+     */
+    void TrafficClass::SetIsDefault(bool default_queue)
     {
-        NS_LOG_WARN("Queue is empty. Cannot remove.");
-        return nullptr;
+        NS_LOG_FUNCTION(this);
+        m_isDefault = default_queue;
     }
 
-    // Remove the packet and update the packet count
-    Ptr<Packet> pkt = m_queue.front();
-    
-    // Pop the packet from the queue and decrement the packet count
-    m_queue.pop();
-    m_packets--;
-    
-    return pkt;
-}
-
-/**
- * \ingroup diffserv
- * \brief Dequeues a packet from the traffic class.
- * \details Pops the next packet and fires the "served" trace callbacks.
- */
-Ptr<Packet> TrafficClass::Dequeue()
-{
-    // Use the silent Remove() to pop the packet
-    Ptr<Packet> pkt = Remove();
-    
-    // Return null pointer if the queue is empty
-    if (!pkt)
+    /** 
+     * \ingroup diffserv
+     * \brief Enqueues a packet into the traffic class.
+     */
+    bool TrafficClass::Enqueue(Ptr<Packet> pkt)
     {
-        NS_LOG_WARN("TrafficClass::Dequeue(): queue is empty");
-        return nullptr;
-    }
-
-    // Log the packet transmission
-    NS_LOG_INFO("TrafficClass::Dequeue(): packet served");
-    
-    return pkt;
-}
-
-/** 
- * \ingroup diffserv
- * \brief Checks if there are no packets.
- */
-bool TrafficClass::IsEmpty() const
-{
-    return m_packets == 0;
-}
-
-/**
- * \ingroup diffserv
- * \brief Getter for maximum number of packets allowed in queue.
- */
-uint32_t
-TrafficClass::GetMaxPackets() const
-{
-    NS_LOG_FUNCTION(this);
-    return m_maxPackets;
-}
-
-/**
- * \ingroup diffserv
- * \brief Checks front packet on the queue.
- */
-Ptr<Packet> TrafficClass::Peek() const
-{
-    // Return null pointer on empty queue
-    if (IsEmpty())
-    {
-        NS_LOG_WARN("Queue is empty. Cannot peek.");
-        return nullptr;
-    }
-
-    // Else, return the front packet
-    Ptr<Packet> pkt = m_queue.front();
-    
-    return pkt;
-}
-
-/**
- * \ingroup diffserv
- * \brief Add a singular filter
- */
-void TrafficClass::AddFilter(Filter *filter)
-{
-    NS_LOG_FUNCTION(this);
-    m_filters.push_back(filter);
-}
-
-/** 
- * \ingroup diffserv
- * \brief Setter for max number of packets
- */
-void TrafficClass::SetMaxPackets(uint32_t max)
-{
-    NS_LOG_FUNCTION(this);
-    m_maxPackets = max;
-}
-
-/**
- * \ingroup diffserv
- * \brief Setter for weight
- */
-void TrafficClass::SetWeight(double weight)
-{
-    NS_LOG_FUNCTION(this);
-    m_weight = weight;
-}
-
-/**
- * \ingroup diffserv
- * \brief Getter for weight
- */
-double TrafficClass::GetWeight() const
-{
-    NS_LOG_FUNCTION(this);
-    return m_weight;
-}
-
-/** 
- * \ingroup diffserv
- * \brief Setter for priority level
- */
-void TrafficClass::SetPriorityLevel(uint32_t lvl)
-{
-    m_priorityLevel = lvl;
-}
-
-/** 
- * \ingroup diffserv
- * \brief Getter for priority level
- */
-uint32_t TrafficClass::GetPriorityLevel() const
-{
-    NS_LOG_FUNCTION(this);
-    return m_priorityLevel;
-}
-
-/** 
- * \ingroup diffserv
- * \brief Matches a packet to each Filter in filters vector.
- */
-bool TrafficClass::Match(Ptr<Packet> pkt) const
-{
-    // If not filters, always match
-    if (m_filters.empty())
-    {
-        NS_LOG_DEBUG("No filters. Always match.");
-        return true;
-    }
-
-    // Else, check each filter
-    for (Filter *filter : m_filters)
-    {
-        // If the filter matches, return true
-        if (filter->Match(pkt))
+        // Add the packet to the queue if it is not full
+        if (m_packets < m_maxPackets)
         {
-            NS_LOG_INFO("Filter matched.");
+            m_queue.push(pkt);
+            m_packets++;
+
             return true;
         }
+
+        NS_LOG_WARN("Queue is full. Packet not enqueued.");
+        return false;
     }
 
-    return false;
-}
+    /**
+     * \ingroup diffserv
+     * \brief Removes a packet from the traffic class.
+     * \details Pops the next packet but does *not* fire the "served" trace logging.
+     * From my research this is the main difference between Dequeue() and Remove().
+     */
+    Ptr<Packet> TrafficClass::Remove()
+    {
+        // Return null pointer on empty queue
+        if (m_queue.empty())
+        {
+            NS_LOG_WARN("Queue is empty. Cannot remove.");
+            return nullptr;
+        }
+
+        // Remove the packet and update the packet count
+        Ptr<Packet> pkt = m_queue.front();
+        
+        // Pop the packet from the queue and decrement the packet count
+        m_queue.pop();
+        m_packets--;
+        
+        return pkt;
+    }
+
+    /**
+     * \ingroup diffserv
+     * \brief Dequeues a packet from the traffic class.
+     * \details Pops the next packet and fires the "served" trace callbacks.
+     */
+    Ptr<Packet> TrafficClass::Dequeue()
+    {
+        // Use the silent Remove() to pop the packet
+        Ptr<Packet> pkt = Remove();
+        
+        // Return null pointer if the queue is empty
+        if (!pkt)
+        {
+            NS_LOG_WARN("TrafficClass::Dequeue(): queue is empty");
+            return nullptr;
+        }
+
+        // Log the packet transmission
+        NS_LOG_INFO("TrafficClass::Dequeue(): packet served");
+        
+        return pkt;
+    }
+
+    /** 
+     * \ingroup diffserv
+     * \brief Checks if there are no packets.
+     */
+    bool TrafficClass::IsEmpty() const
+    {
+        return m_packets == 0;
+    }
+
+    /**
+     * \ingroup diffserv
+     * \brief Getter for maximum number of packets allowed in queue.
+     */
+    uint32_t
+    TrafficClass::GetMaxPackets() const
+    {
+        NS_LOG_FUNCTION(this);
+        return m_maxPackets;
+    }
+
+    /**
+     * \ingroup diffserv
+     * \brief Checks front packet on the queue.
+     */
+    Ptr<Packet> TrafficClass::Peek() const
+    {
+        // Return null pointer on empty queue
+        if (IsEmpty())
+        {
+            NS_LOG_WARN("Queue is empty. Cannot peek.");
+            return nullptr;
+        }
+
+        // Else, return the front packet
+        Ptr<Packet> pkt = m_queue.front();
+        
+        return pkt;
+    }
+
+    /**
+     * \ingroup diffserv
+     * \brief Add a singular filter
+     */
+    void TrafficClass::AddFilter(Filter *filter)
+    {
+        NS_LOG_FUNCTION(this);
+        m_filters.push_back(filter);
+    }
+
+    /** 
+     * \ingroup diffserv
+     * \brief Setter for max number of packets
+     */
+    void TrafficClass::SetMaxPackets(uint32_t max)
+    {
+        NS_LOG_FUNCTION(this);
+        m_maxPackets = max;
+    }
+
+    /**
+     * \ingroup diffserv
+     * \brief Setter for weight
+     */
+    void TrafficClass::SetWeight(double weight)
+    {
+        NS_LOG_FUNCTION(this);
+        m_weight = weight;
+    }
+
+    /**
+     * \ingroup diffserv
+     * \brief Getter for weight
+     */
+    double TrafficClass::GetWeight() const
+    {
+        NS_LOG_FUNCTION(this);
+        return m_weight;
+    }
+
+    /** 
+     * \ingroup diffserv
+     * \brief Setter for priority level
+     */
+    void TrafficClass::SetPriorityLevel(uint32_t lvl)
+    {
+        m_priorityLevel = lvl;
+    }
+
+    /** 
+     * \ingroup diffserv
+     * \brief Getter for priority level
+     */
+    uint32_t TrafficClass::GetPriorityLevel() const
+    {
+        NS_LOG_FUNCTION(this);
+        return m_priorityLevel;
+    }
+
+    /** 
+     * \ingroup diffserv
+     * \brief Matches a packet to each Filter in filters vector.
+     */
+    bool TrafficClass::Match(Ptr<Packet> pkt) const
+    {
+        // If not filters, always match
+        if (m_filters.empty())
+        {
+            NS_LOG_DEBUG("No filters. Always match.");
+            return true;
+        }
+
+        // Else, check each filter
+        for (Filter *filter : m_filters)
+        {
+            // If the filter matches, return true
+            if (filter->Match(pkt))
+            {
+                NS_LOG_INFO("Filter matched.");
+                return true;
+            }
+        }
+
+        return false;
+    }
+} // namespace ns3
